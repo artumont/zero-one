4301bd1122cf6147c3ec3358f334aa6e80f3fb97
artumont <197291181+artumont@users.noreply.github.com>
2026-04-20 20:42:57 -0600

    feat(config): implement create and load methods for GlobalConfig and ProjectConfig
    add functionality to create configuration files if they do not exist
    and load them from a designated directory. Also, ensure the directory
    is created if it doesn't exist.

--- packages/zero-one/src/core/config/global.rs
+++ packages/zero-one/src/core/config/global.rs
@@ -1,26 +1,39 @@
-use std::path::PathBuf;
-
 use serde::{Deserialize, Serialize};
 
 /// A common interface for all configuration objects in the application.
-trait ConfigObject {
-    fn get_docs(&self) -> String;
-}
+// trait ConfigObject {
+//     fn get_docs(&self) -> String;
+// }
 
 /// The struct that holds the application's configuration settings.
 #[derive(Serialize, Deserialize)]
 pub struct GlobalConfig {}
 
 impl GlobalConfig {
-    pub fn save(self, path: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn create(self) -> Result<(), Box<dyn std::error::Error>> {
+        let app_data_dir = crate::utils::ensure_data_directory();
+        let config_path = app_data_dir?.join("global-config.json");
+        if config_path.exists() {
+            return Err("Global configuration already exists.".into());
+        }
+        self.save()?;
+        Ok(())
+    }
+
+    pub fn save(self) -> Result<(), Box<dyn std::error::Error>> {
         let config_str = serde_json::to_string(&self)?;
-        let config_path = path.join("zero-one-config.json");
+        let app_data_dir = crate::utils::ensure_data_directory();
+        let config_path = app_data_dir?.join("global-config.json");
         std::fs::write(config_path, config_str)?;
         Ok(())
     }
 
-    pub fn load(self, path: &PathBuf) -> Result<Self, Box<dyn std::error::Error>> {
-        let config_path = path.join("zero-one-config.json");
+    pub fn load(self) -> Result<Self, Box<dyn std::error::Error>> {
+        let app_data_dir = crate::utils::ensure_data_directory();
+        let config_path = app_data_dir?.join("global-config.json");
+        if !config_path.exists() {
+            return Err("Global configuration not found.".into());
+        }
         let config_str = std::fs::read_to_string(config_path)?;
         let config: Self = serde_json::from_str(&config_str)?;
         Ok(config)
--- packages/zero-one/src/core/config/project.rs
+++ packages/zero-one/src/core/config/project.rs
@@ -3,26 +3,50 @@ use std::path::PathBuf;
 use serde::{Deserialize, Serialize};
 
 /// A common interface for all configuration objects in the application.
-trait ConfigObject {
-    fn get_docs(&self) -> String;
-}
+// trait ConfigObject {
+//     fn get_docs(&self) -> String;
+// }
 
 /// The struct that holds the project configuration settings.
 #[derive(Serialize, Deserialize)]
 pub struct ProjectConfig {}
 
 impl ProjectConfig {
-    pub fn save(self, path: &PathBuf) -> Result<(), Box<dyn std::error::Error>> {
+    pub fn create(self) -> Result<(), Box<dyn std::error::Error>> {
+        let zero_one_dir = ensure_zero_one_dir()?;
+        let config_path = zero_one_dir.join("config.json");
+        if config_path.exists() {
+            return Err("Project configuration already exists.".into());
+        }
+        self.save()?;
+        Ok(())
+    }
+
+    pub fn save(self) -> Result<(), Box<dyn std::error::Error>> {
         let config_str = serde_json::to_string(&self)?;
-        let config_path = path.join("config.json");
+        let zero_one_dir = ensure_zero_one_dir()?;
+        let config_path = zero_one_dir.join("config.json");
         std::fs::write(config_path, config_str)?;
         Ok(())
     }
 
-    pub fn load(self, path: &PathBuf) -> Result<Self, Box<dyn std::error::Error>> {
-        let config_path = path.join("config.json");
+    pub fn load(self) -> Result<Self, Box<dyn std::error::Error>> {
+        let zero_one_dir = ensure_zero_one_dir()?;
+        let config_path = zero_one_dir.join("config.json");
+        if !config_path.exists() {
+            return Err("Project configuration not found.".into());
+        }
         let config_str = std::fs::read_to_string(config_path)?;
         let config: Self = serde_json::from_str(&config_str)?;
         Ok(config)
     }
+}
+
+fn ensure_zero_one_dir() -> Result<PathBuf, Box<dyn std::error::Error>> {
+    let current_dir = std::env::current_dir()?;
+    let zero_one_dir = current_dir.join(".zero-one");
+    if !zero_one_dir.exists() {
+        std::fs::create_dir(&zero_one_dir)?;
+    }
+    Ok(zero_one_dir)
 }
