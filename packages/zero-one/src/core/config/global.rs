60c38988b8b870c984aab91498892b6a555b9939
artumont <197291181+artumont@users.noreply.github.com>
2026-04-21 20:26:32 -0600

    feat(logging): add logging for configuration creation and loading
    Implement logging for errors and successful operations when creating,
    saving, and loading global and project configurations. This enhances
    diagnostics and observability in the configuration management process.

--- packages/zero-one/src/core/config/global.rs
+++ packages/zero-one/src/core/config/global.rs
@@ -16,11 +16,13 @@ impl GlobalConfig {
         let app_data_dir = crate::utils::ensure_data_directory();
         let config_path = app_data_dir?.join("global-config.json");
         if config_path.exists() {
+            log::error!("Global configuration already exists at {:?}", config_path);
             return Err(Box::new(std::io::Error::new(
                 ErrorKind::AlreadyExists,
                 "Global configuration already exists.",
             )));
         }
+        log::debug!("Creating new global configuration: {:?}", self);
         self.save()?;
         Ok(())
     }
@@ -30,6 +32,7 @@ impl GlobalConfig {
         let app_data_dir = crate::utils::ensure_data_directory();
         let config_path = app_data_dir?.join("global-config.json");
         std::fs::write(config_path, config_str)?;
+        log::debug!("Saved global configuration: {:?}", self);
         Ok(())
     }
 
@@ -37,10 +40,12 @@ impl GlobalConfig {
         let app_data_dir = crate::utils::ensure_data_directory();
         let config_path = app_data_dir?.join("global-config.json");
         if !config_path.exists() {
+            log::error!("Global configuration file not found at {:?}", config_path);
             return Err("Global configuration not found.".into());
         }
         let config_str = std::fs::read_to_string(config_path)?;
         let config: Self = serde_json::from_str(&config_str)?;
+        log::debug!("Loaded global configuration: {:?}", config);
         Ok(config)
     }
 }
